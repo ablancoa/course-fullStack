2b91c3583a6c8b41cd6850465811754a1d7a67db
ablancoa <ablancoa95@gmail.com>
2023-04-06 16:05:46 -0400

    [ADD] exercise 1.5 part1

--- part1/part1-app/src/index.js
+++ part1/part1-app/src/index.js
@@ -14,7 +14,8 @@ const Part = ({part, exercises}) => {
 }
 
 const Content = ({parts}) => {
-  return parts.map(part => <Part part={part.name} exercises={part.exercises} />);
+  return parts.map(part => <Part part={part.name} exercises={part.exercises} key={part
+  .name}/>);
 }
 
 const Total = ({parts}) => {
@@ -25,27 +26,29 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
