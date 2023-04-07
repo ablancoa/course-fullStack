b0279a4c0bf329d58a3c86b7552ed69db8f00fe8
ablancoa <ablancoa95@gmail.com>
2023-04-07 19:29:19 -0400

    [ADD] 2.1 course-information-step8-9

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -3,45 +3,68 @@ import Total from "./components/Total";
 
 const Header = ({course}) => {
   return (
-    <h1>{course}</h1>
+    <h2>{course}</h2>
   )
 }
 
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 3,
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
   return (
     <div>
-      <Header course={course.name} />
-      <Course parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <h1>Web development curriculum</h1>
+      {courses.map((item) => (
+        <>
+          <Header course={item.name} />
+          <Course parts={item.parts}/>
+          <Total parts={item.parts}/>
+        </>
+      ))}
     </div>
   )
 }
