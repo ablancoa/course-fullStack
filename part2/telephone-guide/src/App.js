ef4919d1b42d9bb89f746dcb93bf2e130f0f0140
ablancoa <ablancoa95@gmail.com>
2023-04-08 12:13:53 -0400

    [ADD] 2.6 telephone-guide step1

---
+++ part2/telephone-guide/src/App.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleAddContact = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddContact}>
+        <div>
+          name: <input onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App;
