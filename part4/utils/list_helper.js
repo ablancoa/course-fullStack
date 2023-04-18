cbaa36bbeabc06a5962fdcdb0a2bda6802f751b9
ablancoa <ablancoa95@gmail.com>
2023-04-18 18:13:06 -0400

    [ADD] 4.5 unitary test and auxiliary functions step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,4 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.length ===  0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) {
+    return null
+  }
+  else {
+    const favorite = blogs.reduce((prev, curr) => prev.likes > curr.likes ? prev : curr)
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+  }
+
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
